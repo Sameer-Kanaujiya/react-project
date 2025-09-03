c24b19eb7955dd4f1c6a5d2620aa4a05fbda41c4
Sameer Kanaujiya <sameerkanaujiya2004@gmail.com>
2025-09-04 01:31:33 +0530

    add tailwind

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Chai from "./chai";
 
+
 function App() {
   const [counter, sameerCounter] = useState(0); //hooks item
 
@@ -13,7 +14,7 @@ function App() {
   }
   const removevalue = ()=>{
     if (counter==0){
-      sameerCounter=0
+      sameerCounter(0); 
     }else {
       sameerCounter(counter -1);
     }
@@ -27,6 +28,21 @@ function App() {
       <p>Counter value:{counter}</p>
       <button onClick={addvalue}>AddValue</button>
       <button onClick={removevalue}>remove value</button>
+      <div className="flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl">
+        <div>
+          <img className="size-48 shadow-xl rounded-md" alt="" src="https://officetemplatesonline.com/cover-page-templates-formats-for-any-type-of-project/" />
+        </div>
+        <div className="flex items-center md:items-start">
+          <span className="text-2xl font-medium">Class Warfare</span>
+          <span className="font-medium text-sky-500">The Anti-Patterns</span>
+          <span className="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
+            <span>No. 4</span>
+            <span>·</span>
+            <span>2025</span>
+          </span>
+        </div>
+      </div>
+
       
     </>
   );
